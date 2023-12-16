389c4eacf18bfcddea179110fb3d73c94c3ee582
Xavier Ang <xaveang@gmail.com>
2023-12-17 00:17:11 +0800

    Draw image on canvas upon upload (scuffed)

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,5 +1,34 @@
 <script>
     import wasm, { main } from '$lib/init';
+	import { onMount } from 'svelte';
 
-    main(() => wasm.greet('dorb'));
+    /** @type {FileList} */
+    let files;
+
+    /** @type {HTMLCanvasElement} */
+    let canvas;
+
+    /**
+	 * @type {CanvasRenderingContext2D | null}
+	 */
+    let context;
+
+    onMount(() => {
+        context = canvas.getContext('2d');
+    });
+
+    $: if (files) {
+        const img = new Image();
+        img.onload = () => {
+            context?.drawImage(img, 0, 0);
+        };
+        img.src = URL.createObjectURL(files[0]);
+    }
 </script>
+
+<input type="file" multiple accept="image/png, image/jpeg" bind:files />
+
+<div class="h-[100vh] w-[100vw] bg-black">
+    <canvas bind:this={canvas} class="w-full h-full">
+    </canvas>
+</div>
