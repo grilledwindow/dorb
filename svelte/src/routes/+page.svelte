cb68f2f27dcedacf7348992d8964e847889b8dda
Xavier Ang <xaveang@gmail.com>
2023-12-17 17:54:35 +0800

    Improve image scaling on canvas

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -21,8 +21,21 @@
         const img = new Image();
         img.onload = () => {
             if (!context) return;
-            const r = Math.max(img.width / canvas.width, img.height / canvas.height);
-            context.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width / r, img.height / r);
+
+            // set to full width (why doesn't w-full work???)
+            canvas.width = window.innerWidth;
+
+            // ratio of canvas' width to img's width
+            // so we know how to adjust the height accordingly
+            const scale = canvas.width / img.width;
+
+            const scaledImgWidth = img.width * scale;
+            const scaledImgHeight = img.height * scale;
+
+            // adjust canvas' height to scaled img height
+            canvas.height = scaledImgHeight;
+
+            context.drawImage(img, 0, 0, scaledImgWidth, scaledImgHeight);
         };
         img.src = URL.createObjectURL(files[0]);
     }
@@ -30,7 +43,7 @@
 
 <input type="file" multiple accept="image/png, image/jpeg" bind:files />
 
-<div class="h-[100vh] w-[100vw] bg-black">
-    <canvas bind:this={canvas} class="w-full h-full">
+<div class="w-full bg-black">
+    <canvas bind:this={canvas}>
     </canvas>
 </div>
