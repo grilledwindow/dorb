1c60c056af9089e90006dc46750cf7fd147f2c32
Xavier Ang <xaveang@gmail.com>
2023-12-28 22:58:58 +0800

    Page for experimenting with natural HTML elements

---
+++ svelte/src/routes/elem/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import wasm, { main } from '$lib/init';
+
+    /** @type {FileList} */
+    let files;
+
+    /** @type {string[]} */
+    let srcs = [];
+
+    // todo: make this more efficient
+    $: srcs = Array.from(files ?? []).map(URL.createObjectURL);
+</script>
+
+<input type="file" multiple accept="image/png, image/jpeg" bind:files />
+
+<div class="w-full bg-black">
+    {#each srcs as src}
+        <div>
+            <img {src} alt="" />
+        </div>
+    {/each}
+</div>
