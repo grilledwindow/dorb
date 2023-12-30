fb8ddd2d19d3095324d3731cbf53ea87f41057f8
Xavier Ang <xaveang@gmail.com>
2023-12-31 00:34:25 +0800

    Square frames with adjustable padding

--- svelte/src/routes/elem/+page.svelte
+++ svelte/src/routes/elem/+page.svelte
@@ -7,16 +7,22 @@
     /** @type {string[]} */
     let srcs = [];
 
+    /** @type {number} */
+    let width;
+    
+    let padding = 8;
+
     // todo: make this more efficient
     $: srcs = Array.from(files ?? []).map(URL.createObjectURL);
 </script>
 
 <input type="file" multiple accept="image/png, image/jpeg" bind:files />
 
-<div class="w-full bg-black">
+<div class="w-full space-y-2">
+    <input type="number" bind:value={padding}>
     {#each srcs as src}
-        <div>
-            <img {src} alt="" />
+        <div bind:clientWidth={width} class="w-1/2 bg-black flex justify-center items-center" style="height: {width}px">
+            <img {src} alt="" class="left-0" style="max-width: {width - padding * 2}px; max-height: {width - padding * 2}px" />
         </div>
     {/each}
 </div>
