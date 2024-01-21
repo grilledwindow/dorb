20953ed18ccccb9a8950664e65f8bc4ce79c5222
Xavier Ang <xaveang@gmail.com>
2024-01-21 17:45:53 +0800

    Standardise frontend dimensions with backend

--- svelte/src/routes/elem/+page.svelte
+++ svelte/src/routes/elem/+page.svelte
@@ -6,7 +6,8 @@
     let srcs: string[] = [];
     let processedImages: File[] = [];
     let width: number;
-    let padding = 8;
+    let maxSize: number;
+    let padding = 0;
 
     // TODO: make this more efficient
     $: srcs = Array.from(files ?? []).map(URL.createObjectURL);
@@ -23,6 +24,13 @@
         processedImages = await Promise.all(Array.from(files).map(processImage));
         console.log(processedImages)
     };
+
+    $: {
+        const scaledPadding = padding * width / 800 * 2;
+        const frameWidth = width + scaledPadding * 2;
+        const percentage = width / frameWidth;
+        maxSize = width * percentage;
+    }
 </script>
 
 <input type="file" multiple accept="image/png, image/jpeg" bind:files />
@@ -39,7 +47,7 @@
 
     {#each srcs as src}
         <div bind:clientWidth={width} class="w-1/2 bg-black flex justify-center items-center" style="height: {width}px">
-            <img {src} alt="" class="left-0" style="max-width: {width - padding * 2}px; max-height: {width - padding * 2}px" />
+            <img {src} alt="" class="left-0" style="max-width: {maxSize}px; max-height: {maxSize}px" />
         </div>
     {/each}
 </div>
