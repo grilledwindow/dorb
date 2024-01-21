33b88a35296729994fd69fcce994d40d6e4a7056
Xavier Ang <xaveang@gmail.com>
2024-01-21 14:39:00 +0800

    Process images on button click

--- svelte/src/routes/elem/+page.svelte
+++ svelte/src/routes/elem/+page.svelte
@@ -4,36 +4,39 @@
     main(() => {});
     let files: FileList;
     let srcs: string[] = [];
+    let processedImages: File[] = [];
     let width: number;
     let padding = 8;
 
     // TODO: make this more efficient
     $: srcs = Array.from(files ?? []).map(URL.createObjectURL);
-    $: {
-        if (files) {
-            Array.from(files).forEach(async file => {
-                const buf = await file.arrayBuffer();
-                const bytes = new Uint8Array(buf);
-                const newBytes = wasm.files(padding, bytes);
-                const newFile = new File([newBytes], "blah");
 
-                // automatically download new image
-                const aElement = document.createElement('a');
-                aElement.setAttribute('download', 'testphoto.jpg');
-                const href = URL.createObjectURL(newFile);
-                aElement.href = href;
-                aElement.setAttribute('target', '_blank');
-                aElement.click();
-                URL.revokeObjectURL(href);
-            });
-        } 
+    const processImage = async (file: File): Promise<File> => {
+        const buf = await file.arrayBuffer();
+        const bytes = new Uint8Array(buf);
+        const newBytes = wasm.files(padding, bytes);
+        return new File([newBytes], "dorb_" + file.name);
     }
+
+    const processImages = async (files: FileList | null): Promise<File[] | undefined> => {
+        if (!files || !files.length) return;
+        processedImages = await Promise.all(Array.from(files).map(processImage));
+        console.log(processedImages)
+    };
 </script>
 
 <input type="file" multiple accept="image/png, image/jpeg" bind:files />
+<button on:click={() => processImages(files)} class="border border-black p-1">Process images</button>
 
-<div class="w-full space-y-2">
+<div class="w-full mt-4 space-y-2">
     <input type="number" bind:value={padding}>
+    
+    {#each processedImages as img}
+        <a href={URL.createObjectURL(img)} download={img.name} class="border border-black p-1">
+            Download {img.name}
+        </a>
+    {/each}
+
     {#each srcs as src}
         <div bind:clientWidth={width} class="w-1/2 bg-black flex justify-center items-center" style="height: {width}px">
             <img {src} alt="" class="left-0" style="max-width: {width - padding * 2}px; max-height: {width - padding * 2}px" />
